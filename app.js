da127a4e9c04071854e6b0b3a25f4409baef9a94
Naomi Urnes <naomiurnes@gmail.com>
2017-06-07 14:27:10 -0400

    Add up/down buttons

--- app.js
+++ app.js
@@ -10,30 +10,41 @@ const app = {
         const f = ev.target
         const film = {
             name: f.filmName.value,
-            id: this.max+1
+            id: this.max
         }
+
         const fav = document.createElement('button')
         const del = document.createElement('button')
-        fav.addEventListener("click", this.favorite.bind(this));
+        fav.addEventListener("click", this.favorite);
         fav.textContent = "fav"
-        del.addEventListener("click", this.delist.bind(this))
+        del.addEventListener("click", this.delist)
         del.textContent = "del"
+
+        const up = document.createElement('button')
+        const down = document.createElement('button')
+        up.addEventListener("click", this.moveUp.bind(this));
+        up.textContent = "^"
+        down.addEventListener("click", this.moveDown.bind(this))
+        down.textContent = "v"
+
         const li = this.buildLI(film)
         li.appendChild(fav)
         li.appendChild(del)
+        li.appendChild(up)
+        li.appendChild(down)
         this.list.appendChild(li)
-        this.films[this.max] = film
+        this.films[this.max] = film.name
         this.max++
-        console.log(this.films)
     },
     buildLI(film){
         const li = document.createElement('li')
-        li.textContent = film.name
+        const text = document.createElement('span')
+        text.textContent = film.name
+        li.appendChild(text)
         return li
     },
     favorite(ev){
         const film = ev.target.parentElement
-        console.log(film)
         if(film.hasAttribute("class")){
             film.removeAttribute("class")
         }else{
@@ -42,6 +53,28 @@ const app = {
     },
     delist(ev){
         ev.target.parentElement.outerHTML = ""
+    },
+    moveUp(ev){
+        const film = ev.target.parentElement
+        const id = this.films.indexOf(film.childNodes[0].textContent)
+        if(id != 0){
+            this.delist(ev)
+            this.list.insertBefore(film, this.list.childNodes[id-1])
+            const temp = this.films[id]
+            this.films[id] = this.films[id-1]
+            this.films[id-1] = temp
+        }
+    },
+    moveDown(ev){
+        const film = ev.target.parentElement
+        const id = this.films.indexOf(film.childNodes[0].textContent)
+        if(id != this.max-1){
+            this.delist(ev)
+            this.list.insertBefore(film, this.list.childNodes[id+1])
+            const temp = this.films[id]
+            this.films[id] = this.films[id+1]
+            this.films[id+1] = temp
+        }
     },
 }
 
